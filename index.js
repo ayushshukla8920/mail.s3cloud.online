1875e200c608076cb57efc489f814386a8d6c20a
Ayush Shukla <ayushshukla8920@gmail.com>
2025-06-06 00:36:29 +0530

    Added HTTP Auth

--- index.js
+++ index.js
@@ -2,16 +2,22 @@
 const { SMTPServer } = require("smtp-server");
 const { simpleParser } = require('mailparser');
 const express = require('express');
-require('dotenv').config();
 const mongoose = require('mongoose');
 const saveMessage = require('./utils/saveMailToDB');
 const User = require('./models/user');
+const cookieparser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 
 //Database Connection
 mongoose.connect(process.env.MONGOURL)
 .then(()=>console.log("Database Connected Successfully !!"))
 .catch(err=>console.log("Database Connection Error: ",err));
+
+
 
 //SMTP and HTTP Server Setups
 const app = express();
@@ -29,13 +35,13 @@ const server = new SMTPServer({
         User.findOne({ email: recipientEmail })
         .then(user => {
             if (!user) {
-                console.warn(`⛔ Email rejected: No user found for ${recipientEmail}`);
+                console.warn(`Email rejected: No user found for ${recipientEmail}`);
                 return cb(new Error('550 User not found'));
             }
             return cb();
         })
         .catch(err => {
-            console.error("❌ DB Error while checking recipient:", err);
+            console.error("DB Error while checking recipient:", err);
             cb(new Error('451 Temporary server error'));
         });
     },
@@ -47,12 +53,67 @@ const server = new SMTPServer({
             cb();
         })
         .catch(err => {
-            console.error("❌ Failed to parse email:", err);
+            console.error("Failed to parse email:", err);
             cb(err);
         });
     }
 });
 
+
+//Express Middlewares
+app.use(express.json());
+
+
+//HTTP API Routes
+app.post("/api/accounts/login",async(req,res)=>{
+    try{
+        const {email,password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({error: "Email and password are required"})
+        }
+        const result = await User.findOne({email});
+        if(!result){
+            return res.status(400).json({error: "This account doesn't exists"});
+        }
+        const isAuthorised = bcrypt.compareSync(password, result.password);
+        if(!isAuthorised){
+            return res.status(401).json({error: "Incorrect password"});
+        }
+        const token = jwt.sign({email},process.env.JWTSECRET);
+        res.cookie('_authtoken',token,{
+            httpOnly: true,
+            maxAge: 365 * 24 * 60 * 60 * 1000
+        });
+        return res.status(200).json({msg: "Auth Successful"});
+    }
+    catch(error){
+        return res.status(500).json({error: "Internal Server Error"});
+    }
+});
+
+app.post("/api/accounts/create",async(req,res)=>{
+    try{
+        const {name,email,password} = req.body;
+        if(!name || !email || !password){
+            return res.status(400).json({error: "Name,email and password are required"})
+        }
+        const result = await User.findOne({email});
+        if(result){
+            return res.status(401).json({error: "This account already exists"});
+        }
+        const hash = bcrypt.hashSync(password, salt);
+        await User.create({name,email,password: hash});
+        const token = jwt.sign({email},process.env.JWTSECRET);
+        res.cookie('_authtoken',token,{
+            httpOnly: true,
+            maxAge: 365 * 24 * 60 * 60 * 1000
+        });
+        return res.status(201).json({msg: "Creation Successful"});
+    }
+    catch(error){
+        return res.status(500).json({error: "Internal Server Error"});
+    }
+});
 
 //Listening Server on Different Ports
 server.listen(25, () => {
--- models/user.js
+++ models/user.js
@@ -13,10 +13,18 @@ const messageSchema = new mongoose.Schema({
 });
 
 const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
     email: {
         type: String,
         required: true,
         unique: true
+    },
+    password: {
+        type: String,
+        required: true,
     },
     messages: [messageSchema]
 });
